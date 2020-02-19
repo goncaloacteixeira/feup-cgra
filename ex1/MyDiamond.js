dd32907b06c25d9907e1c16baf01e5192f2bc084
skdGT <skidru00@gmail.com>
2020-02-19 09:42:26 +0000

    first commit

---
+++ ex1/MyDiamond.js
@@ -0,0 +1,32 @@
+/**
+ * MyDiamond
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyDiamond extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			-1, 0, 0,	//0
+			0, -1, 0,	//1
+			0, 1, 0,	//2
+			1, 0, 0		//3
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 1, 2,
+			1, 3, 2
+		];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
+
---
+++ ex1/MyInterface.js
@@ -0,0 +1,27 @@
+/**
+* MyInterface
+* @constructor
+*/
+class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        // http://workshop.chromeexperiments.com/examples/gui
+        this.gui = new dat.GUI();
+        
+        var obj = this;
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+
+        //Slider element in GUI
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+
+        return true;
+    }
+}
