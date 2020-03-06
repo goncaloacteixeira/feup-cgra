00b93a79820aa49e445fe305f7e18d28708e5b38
Camões <andre3000pt@gmail.com>
2020-03-06 17:45:54 +0000

    Colors

--- ex2/MyTable.js
+++ ex2/MyTable.js
@@ -8,41 +8,41 @@ class MyTable extends CGFobject {
         super(scene);
 
         this.top1 = new MyUnitCubeQuad(this.scene);
-        this.leg1 = new MyLeg(this.scene);
-        this.leg2 = new MyLeg(this.scene);
-        this.leg3 = new MyLeg(this.scene);
-        this.leg4 = new MyLeg(this.scene);
+        this.leg = new MyLeg(this.scene);
+
     }
 
     display() {
+        this.scene.setDiffuse(0.76,0.60,0.42);
         this.scene.pushMatrix();
         this.scene.translate(0,3.5,0);
         this.scene.scale(20,0.6,10);
         this.top1.display();
         this.scene.popMatrix();
 
+        this.scene.setDiffuse(48/255, 31/255, 13/255);
         this.scene.pushMatrix();
         this.scene.translate(9,0,-4);
         this.scene.scale(1,0.7,1);
-        this.leg1.display();
+        this.leg.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(-9,0,4);
         this.scene.scale(1,0.7,1);
-        this.leg2.display();
+        this.leg.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(-9,0,-4);
         this.scene.scale(1,0.7,1);
-        this.leg3.display();
+        this.leg.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(9,0,4);
         this.scene.scale(1,0.7,1);
-        this.leg4.display();
+        this.leg.display();
         this.scene.popMatrix();
     }
 }
