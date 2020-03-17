98c063c93da48e1e5e13683e11b171658f1f6b05
Camões <andre3000pt@gmail.com>
2020-03-17 09:30:19 +0000

    Experiencias adicionadas + import .js anteriores

---
+++ example4/MyTriangleSmall.js
@@ -0,0 +1,37 @@
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTriangleSmall extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.vertices = [
+            -1, 0, 0,
+            0, 1, 0,
+            1, 0, 0
+        ];
+
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 1, 2,
+            2, 1, 0
+        ];
+
+        this.normals = [
+            0,0,1,
+            0,0,1,
+            0,0,1,
+        ];
+
+        //The defined indices (and corresponding vertices)
+        //will be read in groups of three to draw triangles
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+        this.initGLBuffers();
+    }
+}
+
