dbf73db2f912f621a01db75c19a76f07e1c3fd6b
skdGT <skidru00@gmail.com>
2020-04-25 17:16:20 +0100

    refactoring + model for landed state

--- proj/MySupply.js
+++ proj/MySupply.js
@@ -1,4 +1,3 @@
-
 const SupplyStates = {
     INACTIVE: 0,
     FALLING: 1,
@@ -9,23 +8,31 @@ class MySupply extends CGFobject {
     constructor(scene) {
         super(scene);
         this.box = new MyUnitCubeQuad(this.scene);
-        this.state = SupplyStates.FALLING; // TODO - trocar para INACTIVE quando terminar a modelação
+        this.state = SupplyStates.LANDED; // TODO - trocar para INACTIVE quando terminar a modelação
+
+        this.initMaterials();
     }
 
+    initMaterials() {
+        this.texture = new CGFappearance(this.scene);
+        this.texture.setAmbient(0.3, 0.3, 0.3, 1);
+        this.texture.setDiffuse(0.4, 0.4, 0.4, 1);
+        this.texture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.texture.setShininess(10.0);
+        this.texture.loadTexture('images/supply/crate2.png');
+        this.texture.setTextureWrap('REPEAT', 'REPEAT');
+    }
 
     display() {
-        if (this.state === SupplyStates.FALLING) {
-            this.scene.pushMatrix();
-            this.scene.translate(0, 5, 0);
-            this.box.display(/* adicionar aqui o state que vai fazer a caixa ser desenhada normalmente */);
-            this.scene.popMatrix();
-        }
-        else if (this.state === SupplyStates.LANDED) {
-            this.scene.pushMatrix();
-            this.scene.translate(0, 5, 0);
-            this.box.display(/* adicionar aqui o state que vai fazer a caixa ser desenhada aberta */);
-            this.scene.popMatrix();
-        }
+        this.texture.apply();
+        if (!this.scene.linear)
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
+        else
+            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
+        this.scene.pushMatrix();
+        this.scene.translate(0, 5, 0);
+        this.box.display(this.state);
+        this.scene.popMatrix();
     }
 }
 
--- proj/MyUnitCubeQuad.js
+++ proj/MyUnitCubeQuad.js
@@ -8,62 +8,82 @@ class MyUnitCubeQuad extends CGFobject {
         super(scene);
 
         this.face = new MyQuad(this.scene);
-        this.initMaterials();
-
     }
+
     
-    initMaterials(){
-        this.texture = new CGFappearance(this.scene);
-        this.texture.setAmbient(0.3, 0.3, 0.3, 1);
-        this.texture.setDiffuse(0.4, 0.4, 0.4, 1);
-        this.texture.setSpecular(0.1, 0.1, 0.1, 1);
-        this.texture.setShininess(10.0);
-        this.texture.loadTexture('images/supply/crate1.png');
-        this.texture.setTextureWrap('REPEAT', 'REPEAT');
-    }
-    
-    display() {
+    display(state) {
+        if (state === 1) {
+            // Face da frente
+            this.scene.pushMatrix();
+            this.scene.translate(0, 0, 0.5);
+            this.face.display();
+            this.scene.popMatrix();
 
-        this.texture.apply();
-        if (!this.scene.linear)
-            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
-        else
-            this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.LINEAR);
+            this.scene.pushMatrix();
+            this.scene.rotate(180.0 * Math.PI / 180.0, 0, 1, 0);
+            this.scene.translate(0, 0, 0.5);
+            this.face.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.translate(0,0,0.5);
-        this.face.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.translate(0.5, 0, 0);
+            this.scene.rotate(90.0 * Math.PI / 180.0, 0, 1, 0);
+            this.face.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.rotate(180.0*Math.PI/180.0,0,1,0);
-        this.scene.translate(0,0,0.5);
-        this.face.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.translate(-0.5, 0, 0);
+            this.scene.rotate(-90.0 * Math.PI / 180.0, 0, 1, 0);
+            this.face.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.translate(0.5,0,0);
-        this.scene.rotate(90.0*Math.PI/180.0,0,1,0);
-        this.face.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.translate(0, 0.5, 0);
+            this.scene.rotate(-90.0 * Math.PI / 180.0, 1, 0, 0);
+            this.face.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.translate(-0.5,0,0);
-        this.scene.rotate(-90.0*Math.PI/180.0,0,1,0);
-        this.face.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.translate(0, -0.5, 0);
+            this.scene.rotate(90.0 * Math.PI / 180.0, 1, 0, 0);
+            this.face.display();
+            this.scene.popMatrix();
+        }
 
-        this.scene.pushMatrix();
-        this.scene.translate(0,0.5,0);
-        this.scene.rotate(-90.0*Math.PI/180.0,1,0,0);
-        this.face.display();
-        this.scene.popMatrix();
+        if (state === 2) {
+            this.scene.pushMatrix();
+            this.scene.translate(0, -0.5, 1);
+            this.scene.rotate(Math.PI / 2.0, 1, 0, 0);
+            this.scene.rotate(Math.PI, 0, 1, 0);
+            this.face.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.translate(0,-0.5,0);
-        this.scene.rotate(90.0*Math.PI/180.0,1,0,0);
-        this.face.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.translate(0, -0.5, -1);
+            this.scene.rotate(-Math.PI / 2.0, 1, 0, 0);
+            this.face.display();
+            this.scene.popMatrix();
+
+            this.scene.pushMatrix();
+            this.scene.translate(1, -0.5, 0);
+            this.scene.rotate(Math.PI / 2.0, 0, 0, 1);
+            this.scene.rotate(Math.PI / 2.0, 0, 1, 0);
+            this.face.display();
+            this.scene.popMatrix();
+
+            this.scene.pushMatrix();
+            this.scene.translate(-1, -0.5, 0);
+            this.scene.rotate(-Math.PI / 2.0, 0, 0, 1);
+            this.scene.rotate(-Math.PI / 2.0, 0, 1, 0);
+            this.face.display();
+            this.scene.popMatrix();
+
+            this.scene.pushMatrix();
+            this.scene.translate(0, -0.5, 0);
+            this.scene.rotate(-90.0 * Math.PI / 180.0, 1, 0, 0);
+            this.face.display();
+            this.scene.popMatrix();
+        }
     }
 }
 
